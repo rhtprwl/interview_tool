1d565a232c57a6c39d24e3d221d978840815d74a
sanjayongraph <cs.sanjayyadav@outlook.com>
2014-02-24 13:48:50 +0530

    static pages added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 InterviewTool::Application.routes.draw do
+ get "static_page/home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
