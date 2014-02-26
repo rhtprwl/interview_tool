f9fec069b11462fbd853f04cb68845d939085aa2
Ankita14 <ankiagrawal19@gmail.com>
2014-02-26 16:43:31 +0530

    Admin dashboard added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+	def new
+     @user=User.new
+ 
+    end
+
+    def create
+    	@user = User.new(user_params)    
+    	if @user.save
+    		#redirect_to @user
+      		flash[:success] = "Welcome to the Sample App!"
+      		redirect_to @user
+   		else
+      		render 'new'
+    	end
+    end
+
+    def update
+	  @user = User.find(params[:id])
+	    if @user.update_attributes(user_params)
+	       flash[:success] = "Profile updated"
+	       redirect_to @user
+	      # Handle a successful update.
+	    else
+	      render 'edit'
+	    end
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	private
+
+    def user_params
+      params.require(:user).permit(:name, :password)
+    end
+end
