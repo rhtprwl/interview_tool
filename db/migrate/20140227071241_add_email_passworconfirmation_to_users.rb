c926624f115ac26fa3ce32a520a15fdf3cb1fb17
sanjayongraph <cs.sanjayyadav@outlook.com>
2014-02-28 19:15:26 +0530

    removing migration again

--- db/migrate/20140227071241_add_email_passworconfirmation_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddEmailPassworconfirmationToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :password_confirmation, :string
-    add_column :users, :email, :role
-  end
-end
