96efa2a2955c5fda6533519896ce48cc0b055430
Ankita14 <ankiagrawal19@gmail.com>
2014-03-19 19:19:14 +0530

    mistakely added two migration of same name

--- db/migrate/20140305071014_add_options_to_add_questions.rb
+++
@@ -1,9 +0,0 @@
-class AddOptionsToAddQuestions < ActiveRecord::Migration
-  def change
-    add_column :add_questions, :option1, :string
-    add_column :add_questions, :option2, :string
-    add_column :add_questions, :option3, :string
-    add_column :add_questions, :option4, :string
-    add_column :add_questions, :answer, :string
-  end
-end
